578574cafd60ad5a6d2d5490ce773a33ad5c6f61
Felipe <felipeyutaka10@gmail.com>
2022-09-14 22:46:27 -0300

    Implementando Specification

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries{
+public interface RestauranteRepository 
+		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
+		JpaSpecificationExecutor<Restaurante>{
 
 }
